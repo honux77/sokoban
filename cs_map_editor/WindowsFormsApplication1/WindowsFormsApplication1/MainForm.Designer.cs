e1ca46a11f1cbcd97b16888e66dfbc478cf32b00
horong54 <k502000@naver.com>
2013-10-31 14:59:21 +0900

    map editor 0.1

--- cs_map_editor/WindowsFormsApplication1/WindowsFormsApplication1/MainForm.Designer.cs
+++ cs_map_editor/WindowsFormsApplication1/WindowsFormsApplication1/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApplication1
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -30,51 +30,77 @@
         {
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.buttonResize = new System.Windows.Forms.Button();
+            this.textBoxH = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxW = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.mapDisplay = new System.Windows.Forms.PictureBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mapDisplay)).BeginInit();
             this.SuspendLayout();
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Image = global::WindowsFormsApplication1.Properties.Resources.wall;
-            this.radioButton1.Location = new System.Drawing.Point(18, 20);
+            this.radioButton1.Location = new System.Drawing.Point(18, 56);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(44, 30);
             this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
             this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged_1);
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.radioButton5);
             this.groupBox1.Controls.Add(this.radioButton4);
             this.groupBox1.Controls.Add(this.radioButton3);
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Location = new System.Drawing.Point(441, 186);
+            this.groupBox1.Location = new System.Drawing.Point(512, 186);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(222, 69);
+            this.groupBox1.Size = new System.Drawing.Size(222, 102);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "지형선택";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Image = global::WindowsFormsApplication1.Properties.Resources._char;
+            this.radioButton4.Location = new System.Drawing.Point(168, 20);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(44, 30);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Image = global::WindowsFormsApplication1.Properties.Resources.bbox;
+            this.radioButton3.Location = new System.Drawing.Point(118, 20);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(44, 30);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
             // radioButton2
             // 
@@ -86,28 +112,7 @@
             this.radioButton2.TabIndex = 1;
             this.radioButton2.TabStop = true;
             this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Image = global::WindowsFormsApplication1.Properties.Resources.bbox;
-            this.radioButton3.Location = new System.Drawing.Point(118, 20);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(44, 30);
-            this.radioButton3.TabIndex = 2;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // radioButton4
-            // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Image = global::WindowsFormsApplication1.Properties.Resources._char;
-            this.radioButton4.Location = new System.Drawing.Point(168, 20);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(44, 30);
-            this.radioButton4.TabIndex = 3;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // comboBox1
             // 
@@ -120,7 +125,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(439, 62);
+            this.label1.Location = new System.Drawing.Point(510, 62);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 12);
             this.label1.TabIndex = 3;
@@ -148,7 +153,7 @@
             this.groupBox2.Controls.Add(this.comboBox1);
             this.groupBox2.Controls.Add(this.button1);
             this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Location = new System.Drawing.Point(441, 16);
+            this.groupBox2.Location = new System.Drawing.Point(512, 16);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(222, 81);
             this.groupBox2.TabIndex = 6;
@@ -157,42 +162,36 @@
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.buttonResize);
+            this.groupBox3.Controls.Add(this.textBoxH);
             this.groupBox3.Controls.Add(this.label4);
             this.groupBox3.Controls.Add(this.label3);
             this.groupBox3.Controls.Add(this.label2);
-            this.groupBox3.Controls.Add(this.textBox1);
-            this.groupBox3.Location = new System.Drawing.Point(441, 104);
+            this.groupBox3.Controls.Add(this.textBoxW);
+            this.groupBox3.Location = new System.Drawing.Point(512, 104);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(222, 66);
             this.groupBox3.TabIndex = 7;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "맵크기";
             // 
-            // textBox1
+            // buttonResize
             // 
-            this.textBox1.Location = new System.Drawing.Point(27, 32);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(40, 21);
-            this.textBox1.TabIndex = 0;
+            this.buttonResize.Location = new System.Drawing.Point(139, 32);
+            this.buttonResize.Name = "buttonResize";
+            this.buttonResize.Size = new System.Drawing.Size(75, 23);
+            this.buttonResize.TabIndex = 5;
+            this.buttonResize.Text = "Resize";
+            this.buttonResize.UseVisualStyleBackColor = true;
+            this.buttonResize.Click += new System.EventHandler(this.button4_Click);
             // 
-            // label2
+            // textBoxH
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 17);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(15, 12);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "W";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(73, 38);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(13, 12);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "X";
+            this.textBoxH.Location = new System.Drawing.Point(92, 32);
+            this.textBoxH.Name = "textBoxH";
+            this.textBoxH.Size = new System.Drawing.Size(40, 21);
+            this.textBoxH.TabIndex = 4;
+            this.textBoxH.Text = "10";
             // 
             // label4
             // 
@@ -203,24 +202,35 @@
             this.label4.TabIndex = 3;
             this.label4.Text = "H";
             // 
-            // textBox2
+            // label3
             // 
-            this.textBox2.Location = new System.Drawing.Point(92, 32);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(40, 21);
-            this.textBox2.TabIndex = 4;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "X";
             // 
-            // pictureBox1
+            // label2
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 16);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(421, 239);
-            this.pictureBox1.TabIndex = 8;
-            this.pictureBox1.TabStop = false;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "W";
+            // 
+            // textBoxW
+            // 
+            this.textBoxW.Location = new System.Drawing.Point(27, 32);
+            this.textBoxW.Name = "textBoxW";
+            this.textBoxW.Size = new System.Drawing.Size(40, 21);
+            this.textBoxW.TabIndex = 0;
+            this.textBoxW.Text = "16";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(208, 281);
+            this.button3.Location = new System.Drawing.Point(302, 379);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 9;
@@ -228,25 +238,62 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // Form1
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(209, 379);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "위치표시라벨";
+            // 
+            // mapDisplay
+            // 
+            this.mapDisplay.BackColor = System.Drawing.SystemColors.Window;
+            this.mapDisplay.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.floor;
+            this.mapDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mapDisplay.Location = new System.Drawing.Point(26, 16);
+            this.mapDisplay.Name = "mapDisplay";
+            this.mapDisplay.Size = new System.Drawing.Size(480, 300);
+            this.mapDisplay.TabIndex = 11;
+            this.mapDisplay.TabStop = false;
+            this.mapDisplay.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mapDisplay_MouseDown);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Checked = true;
+            this.radioButton5.Image = global::WindowsFormsApplication1.Properties.Resources.floor;
+            this.radioButton5.Location = new System.Drawing.Point(18, 20);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(44, 30);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(675, 323);
+            this.ClientSize = new System.Drawing.Size(742, 416);
+            this.Controls.Add(this.mapDisplay);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
-            this.Name = "Form1";
+            this.DoubleBuffered = true;
+            this.Name = "MainForm";
             this.Text = "맵에디터";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mapDisplay)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -265,13 +312,16 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxH;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxW;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonResize;
+        private System.Windows.Forms.PictureBox mapDisplay;
+        private System.Windows.Forms.RadioButton radioButton5;
     }
 }
 
---
+++ cs_map_editor/WindowsFormsApplication1/WindowsFormsApplication1/MainForm.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    enum TILETYPE { FLOOR=0, WALL,BBOX, GOAL, CHARA};
+    public partial class MainForm : Form
+    {
+        public Bitmap bmFloor;
+        public Bitmap bmBbox;
+        public Bitmap bmGoal;
+        public Bitmap bmWall;
+        public Bitmap bmChara;
+        const int TSIZE = 30; //tile size
+        const int MAXW = 16;
+        const int MAXH = 10;
+        TILETYPE tileType = TILETYPE.FLOOR;
+        TILETYPE[,] map;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBoxW.Text);
+            int y = int.Parse(textBoxH.Text);
+            mapDisplay.Width = x * 30;
+            mapDisplay.Height = y * 30;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            bmFloor = new Bitmap(WindowsFormsApplication1.Properties.Resources.floor);
+            bmBbox = new Bitmap(WindowsFormsApplication1.Properties.Resources.bbox);
+            bmGoal = new Bitmap(WindowsFormsApplication1.Properties.Resources.goal);
+            bmWall = new Bitmap(WindowsFormsApplication1.Properties.Resources.wall);
+            bmChara = new Bitmap(WindowsFormsApplication1.Properties.Resources._char);
+
+            map = new TILETYPE[MAXH, MAXW];
+            for (int y = 0; y < MAXH; y++)
+                for (int x = 0; x < MAXW; x++)
+                    map[y, x] = TILETYPE.FLOOR;
+        }
+
+        private void mapDisplay_MouseDown(object sender, MouseEventArgs e)
+        {
+            label5.Text = e.X + " : " + e.Y;
+            int w =  e.X / TSIZE;
+            int h = e.Y / TSIZE;
+            map[h, w] = tileType;
+
+            Graphics g = mapDisplay.CreateGraphics();
+            for (int y = 0; y < MAXH; y++)
+                for (int x = 0; x < MAXW; x++)
+                {
+                    int X = x * TSIZE;
+                    int Y = y * TSIZE;
+                    switch (map[y, x])
+                    {
+                        case TILETYPE.FLOOR:
+                            g.DrawImage(bmFloor, new Point(X, Y));
+                            break;
+                        case TILETYPE.WALL:
+                            g.DrawImage(bmWall, new Point(X, Y));
+                            break;
+                        case TILETYPE.BBOX:
+                            g.DrawImage(bmBbox, new Point(X, Y));
+                            break;
+                        case TILETYPE.GOAL:
+                            g.DrawImage(bmGoal, new Point(X, Y));
+                            break;
+                        case TILETYPE.CHARA:
+                            g.DrawImage(bmChara, new Point(X, Y));
+                            break;
+                    }
+                }
+            g.Dispose(); 
+        }
+
+        private void radioButton1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            tileType = TILETYPE.WALL;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            tileType = TILETYPE.GOAL;
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            tileType = TILETYPE.FLOOR;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            tileType = TILETYPE.BBOX;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            tileType = TILETYPE.CHARA;
+        }
+    
+    }
+}
